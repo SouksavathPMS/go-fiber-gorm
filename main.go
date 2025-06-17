20eaf094d1bd2c5d63a8d1a4c17d6b61779a2fe4
Souksavath Phommaseng <kyedev@Souksavaths-MacBook-Pro.local>
2025-06-17 11:50:17 +0700

    Finish all the CRUD function but have not connect to postgres yet (I'll leave it here)

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/SouksavathPMS/go-fiber-postgres/models"
+	"github.com/SouksavathPMS/go-fiber-postgres/storage"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"gorm.io/gorm"
@@ -50,6 +52,52 @@ func (r *Repository) GetBooks(context *fiber.Ctx) error {
 	return nil
 }
 
+// DeleteBook handles /delete-book/:{id} for delete the book
+func (r *Repository) DeleteBook(context *fiber.Ctx) error {
+	bookModel := &models.Books{}
+	id := context.Params("id")
+	if id == "" {
+		context.Status(http.StatusInternalServerError).JSON(
+			&fiber.Map{"message": "Book not found"},
+		)
+		return nil
+	}
+	err := r.DB.Delete(bookModel, id)
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "Could not delete book"},
+		)
+		return err.Error
+	}
+	context.Status(http.StatusOK).JSON(
+		&fiber.Map{"message": "Book delete successfully"},
+	)
+	return nil
+}
+
+// GetBookByID handles /get-books/:{id} which will get the book info by its ID
+func (r *Repository) GetBookByID(context *fiber.Ctx) error {
+	bookModel := &models.Books{}
+	id := context.Params("id")
+	if id == "" {
+		context.Status(http.StatusInternalServerError).JSON(
+			&fiber.Map{"message": "Book not found"},
+		)
+		return nil
+	}
+	err := r.DB.Where("id = ?", id).First(bookModel).Error
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "Could not get the book with the ID"},
+		)
+		return err
+	}
+	context.Status(http.StatusOK).JSON(
+		&fiber.Map{"message": "Fected book with the ID successfully", "data": bookModel},
+	)
+	return nil
+}
+
 func (r *Repository) SetupRoutes(app *fiber.App) {
 	api := app.Group("/api")
 	api.Post("/create-books", r.CreateBook)
@@ -65,9 +113,23 @@ func main() {
 		log.Fatalf("Error while loading .evn with: %s", err)
 	}
 
+	config := &storage.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Password: os.Getenv("DB_PASSWORD"),
+		User:     os.Getenv("DB_USER"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+		DBName:   os.Getenv("DB_NAME"),
+	}
+
 	// Storage connection
-	if db, err := storage.NewConnection(config); err != nil {
+	db, err := storage.NewConnection(config)
+	if db != nil {
 		log.Fatal(err)
+	}
+
+	if err = models.MigrateBooks(db); err != nil {
+		log.Fatal("Could not migrate DB")
 	}
 
 	r := Repository{
--- models/books.go
+++ models/books.go
@@ -1,7 +1,15 @@
 package models
 
+import "gorm.io/gorm"
+
 type Books struct {
-	Title     string `json:"title"`
-	Author    string `json:"author"`
-	Publisher string `json:"publisher"`
+	ID        uint    `gorm:"primaryKey;autoIncrement:true"`
+	Title     *string `json:"title"`
+	Author    *string `json:"author"`
+	Publisher *string `json:"publisher"`
+}
+
+func MigrateBooks(db *gorm.DB) error {
+	err := db.AutoMigrate(&Books{})
+	return err
 }
